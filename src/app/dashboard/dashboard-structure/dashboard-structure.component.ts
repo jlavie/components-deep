d650afcf924c37ff03e70e3948472557f6d8c5e7
Lardon <lavie.jul@gmail.com>
2024-11-28 15:50:32 +0100

    add class on host element who is encapsulate

--- src/app/dashboard/dashboard-structure/dashboard-structure.component.ts
+++ src/app/dashboard/dashboard-structure/dashboard-structure.component.ts
@@ -1,11 +1,15 @@
-import { Component, input, Input } from '@angular/core';
+import { Component, input, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-structure',
   standalone: true,
   imports: [],
   templateUrl: './dashboard-structure.component.html',
-  styleUrl: './dashboard-structure.component.css'
+  styleUrl: './dashboard-structure.component.css',
+  host: {
+    class: 'dashboard-item'
+  },
+  encapsulation: ViewEncapsulation.None
 })
 export class DashboardStructureComponent {
   // @Input({required: true}) image!: { source: string; alt: string; };
