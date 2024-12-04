45af71452363fec85cb10034d31b0032a2420943
Lardon <lavie.jul@gmail.com>
2024-12-05 00:54:39 +0100

    rewrite custom 2way binding with the new function model

--- src/app/rect/display/display.component.ts
+++ src/app/rect/display/display.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, model, output } from '@angular/core';
 
 @Component({
   selector: 'app-display',
@@ -9,11 +9,17 @@ import { Component, input, output } from '@angular/core';
 })
 export class DisplayComponent {
   // Todo: Implement custom two-way binding
-  size = input.required<{width: string; height: string;}>();
-  sizeChange = output<{width: string; height: string;}>();// la sortie doit avoir le même nom que l'entré + Change
+  // size = input.required<{width: string; height: string;}>();
+  // sizeChange = output<{width: string; height: string;}>();// la sortie doit avoir le même nom que l'entré + Change
+
+  size = model.required<{width: string; height: string;}>()
 
   onReset() {
-    this.sizeChange.emit({
+    // this.sizeChange.emit({
+    //   width: '200',
+    //   height: '100'
+    // })
+    this.size.set({
       width: '200',
       height: '100'
     })
