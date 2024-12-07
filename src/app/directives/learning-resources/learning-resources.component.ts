fef8329ff0f0321f53c299a62c1e3195241130f2
Lardon <lavie.jul@gmail.com>
2024-12-07 11:51:45 +0100

    add custom structural directive for external links

--- src/app/directives/learning-resources/learning-resources.component.ts
+++ src/app/directives/learning-resources/learning-resources.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SafeLinkfDirective } from '../../shared/directives/safe-link.directive';
 
 @Component({
   selector: 'app-learning-resources',
   standalone: true,
-  imports: [],
+  imports: [SafeLinkfDirective],
   templateUrl: './learning-resources.component.html',
   styleUrl: './learning-resources.component.css'
 })
