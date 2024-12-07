ab83126a9dd59fabca5a32e41cd8818c72a5d39c
Lardon <lavie.jul@gmail.com>
2024-12-07 11:10:09 +0100

    conditionnal display with signal or structural directive

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { AuthComponent } from './auth/auth.component';
 import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
+import { AuthService } from './auth/auth.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-directives',
   standalone: true,
-  imports: [AuthComponent, LearningResourcesComponent],
+  imports: [AuthComponent, LearningResourcesComponent, NgIf],
   templateUrl: './directives.component.html',
   styleUrl: './directives.component.css'
 })
 export class DirectivesComponent {
+  private authService = inject(AuthService);
 
+  isAdmin = computed(() => this.authService.activePermission() === 'admin');
 }
