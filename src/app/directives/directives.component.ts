8a39ad41c4af3651731a766e46a870b06e2b3f7c
Lardon <lavie.jul@gmail.com>
2024-12-08 12:20:22 +0100

    create new directive for conditionnal content within permission

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -3,11 +3,12 @@ import { AuthComponent } from './auth/auth.component';
 import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
 import { AuthService } from './auth/auth.service';
 import { NgIf } from '@angular/common';
+import { AuthDirective } from '../shared/directives/auth.directive';
 
 @Component({
   selector: 'app-directives',
   standalone: true,
-  imports: [AuthComponent, LearningResourcesComponent, NgIf],
+  imports: [AuthComponent, LearningResourcesComponent, NgIf, AuthDirective],
   templateUrl: './directives.component.html',
   styleUrl: './directives.component.css'
 })
