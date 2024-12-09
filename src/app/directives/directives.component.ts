b4fc17b284b780d8f202171c71b4899cb1af438e
Lardon <lavie.jul@gmail.com>
2024-12-09 09:22:25 +0100

    add log directive and call her by 2 ways : host parameter or directly

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -4,11 +4,12 @@ import { LearningResourcesComponent } from './learning-resources/learning-resour
 import { AuthService } from './auth/auth.service';
 import { NgIf } from '@angular/common';
 import { AuthDirective } from '../shared/directives/auth.directive';
+import { LogDirective } from '../shared/directives/log.directive';
 
 @Component({
   selector: 'app-directives',
   standalone: true,
-  imports: [AuthComponent, LearningResourcesComponent, NgIf, AuthDirective],
+  imports: [AuthComponent, LearningResourcesComponent, NgIf, AuthDirective, LogDirective],
   templateUrl: './directives.component.html',
   styleUrl: './directives.component.css'
 })
--- src/app/directives/learning-resources/learning-resources.component.ts
+++ src/app/directives/learning-resources/learning-resources.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SafeLinkfDirective } from '../../shared/directives/safe-link.directive';
+import { LogDirective } from '../../shared/directives/log.directive';
 
 @Component({
   selector: 'app-learning-resources',
   standalone: true,
-  imports: [SafeLinkfDirective],
+  imports: [SafeLinkfDirective, LogDirective],
   templateUrl: './learning-resources.component.html',
   styleUrl: './learning-resources.component.css'
 })
