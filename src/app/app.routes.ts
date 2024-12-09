51334384309425b5aa6da089aa62b8918cfe964a
Lardon <lavie.jul@gmail.com>
2024-12-09 14:04:10 +0100

    add component for working with pipes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LifecycleComponent } from './lifecycle/lifecycle.component';
 import { AdministrationComponent } from './administration/administration.component';
 import { RectComponent } from './rect/rect.component';
 import { DirectivesComponent } from './directives/directives.component';
+import { PipesComponent } from './pipes/pipes.component';
 
 export const routes: Routes = [
     { path: '',   redirectTo: '/administration', pathMatch: 'full' },
@@ -10,4 +11,5 @@ export const routes: Routes = [
     { path: 'lifecycle', component: LifecycleComponent },
     { path: 'rec', component: RectComponent },
     { path: 'directives', component: DirectivesComponent },
+    { path: 'pipes', component: PipesComponent },
   ];
---
+++ src/app/pipes/pipes.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pipes',
+  standalone: true,
+  imports: [],
+  templateUrl: './pipes.component.html',
+  styleUrl: './pipes.component.css'
+})
+export class PipesComponent {
+  currentDate = new Date();
+  currentTemperaturs = {
+    berlin: 4.2749812,
+    newYork: 18.1214,
+    paris: 72.1209001,
+    chicago: 65.0775238,
+  };
+
+  historicTemperatures = [
+    25, 37, 19, -4, 28, 21, 19, 28, 33, 31, 9, 11, 5, -12, -5,
+  ];
+
+  onReset(index: number) {
+    this.historicTemperatures[index] = 18;
+  }
+}
